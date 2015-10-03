a3d3bc6af03bc1c398b9cb61ce3670a6025b8e6e
mbelkin <michael@belkininteractive.com>
2015-10-03 13:44:05 -0700

    need to call server.listen to handle requests

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-// server.listen(port, function () {
-//   console.log('Server listening at port %d', port);
-// });
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
 
 // Routing
 //app.use(express.static(__dirname + '/public'));
