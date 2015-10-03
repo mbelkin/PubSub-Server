78b2baa9013b25ab07edc080b53791ba30484ae7
mbelkin <michael@belkininteractive.com>
2015-10-03 12:13:58 -0700

    trying azure socket.io chat demo example

--- app.js
+++ app.js
@@ -1,43 +1,79 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var bodyParser = require('body-parser');
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
-
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
 });
 
-app.post('/publish/:channel', function (req, res) {
-	var channel = req.params.channel;
-	var message = JSON.stringify(req.body);
+// Routing
+app.use(express.static(__dirname + '/public'));
 
-	io.emit(channel, message);
-	res.send('publishing to channel: ' + channel + '\n' + message);
+// Chatroom
 
+// usernames which are currently connected to the chat
+var usernames = {};
+var numUsers = 0;
 
-	// var message = req.body;
-	// console.log(req.body);
-	// var name = message["name"];
-	// res.send('name: ' + name);
-	//console.log(message);
-  	//res.json(message);
-	//res.send(message);
-  //res.send('POST request to the homepage');
-});
+io.on('connection', function (socket) {
+  var addedUser = false;
 
-function publish(channel, message) {
-	
-}
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-  	io.emit('chat message', msg);
-    console.log('message: ' + msg);
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
   });
-});
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    if (addedUser) {
+      delete usernames[socket.username];
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
 });
